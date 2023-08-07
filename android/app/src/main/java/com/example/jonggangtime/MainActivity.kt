8668509e2586471c636098ebc3a6f509c6fd658b
lee-june-young <uhiccup@gmail.com>
2023-08-08 01:00:11 +0900

    Style: BaseTemplate

---
+++ android/app/src/main/java/com/example/jonggangtime/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.jonggangtime
+
+import com.example.jonggangtime.Utils.BaseActivity
+import com.example.jonggangtime.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+
+    override fun initAfterBinding() {
+
+    }
+
+}
---
+++ android/app/src/main/java/com/example/jonggangtime/Utils/ViewUtils.kt
@@ -0,0 +1,6 @@
+package com.example.jonggangtime.Utils
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+
+typealias Inflate<T> = (LayoutInflater, ViewGroup?, Boolean) -> T
