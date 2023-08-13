bf7f23b90099042e6214b1408597018151201c42
JooeonLee <cacophony0609@gmail.com>
2023-08-13 17:39:25 +0900

    Feat (TimeTable) : TimeTable 도메인에 필드값 추가

--- server/src/main/java/com/example/server/domain/TimeTable.java
+++ server/src/main/java/com/example/server/domain/TimeTable.java
@@ -1,4 +1,25 @@
 package com.example.server.domain;
 
-public class TimeTable {
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class TimeTable extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
 }
