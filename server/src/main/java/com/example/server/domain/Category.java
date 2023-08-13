c74397f4aa3dc5a88d0b944215162b08e6f0fc17
JooeonLee <cacophony0609@gmail.com>
2023-08-13 17:01:33 +0900

    Feat (Category) : 카테고리 도메인이 BaseTimeEntity extends 하게 변경

--- server/src/main/java/com/example/server/domain/Category.java
+++ server/src/main/java/com/example/server/domain/Category.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class Category {
+public class Category extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
