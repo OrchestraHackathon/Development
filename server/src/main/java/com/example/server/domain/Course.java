852f4478b2e0555c02c1ef407a28e3928663617c
JooeonLee <cacophony0609@gmail.com>
2023-08-13 17:05:59 +0900

    Feat (Course) : Course 도메인의 필드값 추가

--- server/src/main/java/com/example/server/domain/Course.java
+++ server/src/main/java/com/example/server/domain/Course.java
@@ -1,4 +1,29 @@
 package com.example.server.domain;
 
-public class Course {
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Course extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String detail;
+    @Column(nullable = false)
+    private String summary;
+    @Column(nullable = false)
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
 }
