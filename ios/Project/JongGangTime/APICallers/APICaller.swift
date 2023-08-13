9541feba3904f9c2aa74239dc1eda2ec36e6c977
encoreJeong <zzaibean@gmail.com>
2023-08-13 23:04:26 +0900

    Add (CourseTableViewController.swift) : 코스 둘러보기 화면 추가

---
+++ ios/Project/JongGangTime/APICallers/APICaller.swift
@@ -0,0 +1,8 @@
+//
+//  APICaller.swift
+//  JongGangTime
+//
+//  Created by 정재연 on 2023/08/13.
+//
+
+import Foundation
---
+++ ios/Project/JongGangTime/TableViewCells/CourseTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CourseTableViewCell.swift
+//  JongGangTime
+//
+//  Created by 정재연 on 2023/08/13.
+//
+
+import UIKit
+
+class CourseTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
