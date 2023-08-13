f544f781ffdd5fedd73eb4eea78c7c8db4ad7793
JooeonLee <cacophony0609@gmail.com>
2023-08-13 17:33:59 +0900

    Feat (TimeTableCourseDetail) : TimeTableCourseDetail 도메인에 필드값 추가

--- server/src/main/java/com/example/server/domain/TimeTableCourseDetail.java
+++ server/src/main/java/com/example/server/domain/TimeTableCourseDetail.java
@@ -1,4 +1,22 @@
 package com.example.server.domain;
 
-public class TimeTableCourseDetail {
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class TimeTableCourseDetail extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
 }
