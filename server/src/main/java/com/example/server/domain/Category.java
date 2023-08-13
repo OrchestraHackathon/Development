a87f156eba60ae0206ee82865f63839f728ee077
JooeonLee <cacophony0609@gmail.com>
2023-08-13 16:58:53 +0900

    Feat (Category) : Category 도메인의 필드 생성

--- server/src/main/java/com/example/server/domain/Category.java
+++ server/src/main/java/com/example/server/domain/Category.java
@@ -1,4 +1,25 @@
 package com.example.server.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
 }
