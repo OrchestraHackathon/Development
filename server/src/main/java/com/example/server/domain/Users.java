babd7a9691e50515ff0fad0aee4d57a5044da656
JooeonLee <cacophony0609@gmail.com>
2023-08-13 16:51:27 +0900

    Feat (Users) : Users 도메인 필드 생성

--- server/src/main/java/com/example/server/domain/Users.java
+++ server/src/main/java/com/example/server/domain/Users.java
@@ -1,4 +1,29 @@
 package com.example.server.domain;
 
-public class Users {
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Where;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Users extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
 }
