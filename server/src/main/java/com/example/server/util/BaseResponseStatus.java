7a442a593c6b385b799925e05a5f9b1e581ca4dd
JooeonLee <cacophony0609@gmail.com>
2023-08-14 17:38:31 +0900

    Feat (BaseResponseStatus) : BaseResponseStatus에 SUCCESS status 추가

--- server/src/main/java/com/example/server/util/BaseResponseStatus.java
+++ server/src/main/java/com/example/server/util/BaseResponseStatus.java
@@ -1,4 +1,38 @@
 package com.example.server.util;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum BaseResponseStatus {
+
+    /**
+     * 1000 : 요청 성공
+     */
+    SUCCESS(true, 1000, "요청에 성공하였습니다.");
+
+    /**
+     * 2000 : Request 오류
+     */
+
+    /**
+     * 3000 : Response 오류
+     */
+
+    /**
+     * 4000 : Database, Server 오류
+     */
+
+    /**
+     * 5000 : AWS 오류
+     */
+
+    /**
+     * 6000 : 보안 이슈
+     */
+
+    private final boolean isSuccess;
+    private final int responseCode;
+    private final String responseMessage;
 }
