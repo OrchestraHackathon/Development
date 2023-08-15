1e3e0589a1b357b7a5e23290fbb551debaa4016f
JooeonLee <cacophony0609@gmail.com>
2023-08-15 11:48:38 +0900

    Feat (UsersSignInRequestDto) : dto 에 필드값과 toEntity 메서드 추가

--- server/src/main/java/com/example/server/dto/users/UsersSignInRequestDto.java
+++ server/src/main/java/com/example/server/dto/users/UsersSignInRequestDto.java
@@ -1,5 +1,31 @@
 package com.example.server.dto.users;
 
+import com.example.server.domain.Status;
+import com.example.server.domain.users.Users;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class UsersSignInRequestDto {
+
+    private String email;
+
+    private String password;
+
+    private String userName;
+
+    private String userNickName;
+
+    // dto -> entity
+    public Users toEntity() {
+        return Users.builder()
+                .email(email)
+                .password(password)
+                .name(userName)
+                .nickname(userNickName)
+                .status(Status.ACTIVE)
+                .role("USER")
+                .build();
+    }
 }
