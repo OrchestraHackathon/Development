28d0a92a90cb41ba1db9c77b95d6f74b4c50cdb3
JooeonLee <cacophony0609@gmail.com>
2023-08-15 12:48:27 +0900

    Feat (PrincipalDetailsService) : PrincipalDetailsService 추가

--- server/src/main/java/com/example/server/service/auth/PrincipalDetailsService.java
+++ server/src/main/java/com/example/server/service/auth/PrincipalDetailsService.java
@@ -1,4 +1,30 @@
 package com.example.server.service.auth;
 
-public class PrincipalDetailsService {
+import com.example.server.domain.users.PrincipalDetails;
+import com.example.server.domain.users.Users;
+import com.example.server.repository.UsersRepository;
+import com.example.server.util.exception.UsersException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import static com.example.server.util.BaseResponseStatus.*;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UsersRepository usersRepository;
+
+    @Override
+    public PrincipalDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Users users = usersRepository.findByEmail(email)
+                .orElseThrow(() -> new UsersException(NONE_USER));
+        log.info("loadUserByUsername, user=[{}][{}]", users.getEmail(), users.getRole());
+        return new PrincipalDetails(users);
+    }
 }
