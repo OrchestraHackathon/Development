b32820ed04ea9a3684e9375e850cab386b5e41db
JooeonLee <cacophony0609@gmail.com>
2023-08-15 11:57:22 +0900

    Feat (UsersService) : 서비스단에 회원가입 로직 추가

--- server/src/main/java/com/example/server/service/UsersService.java
+++ server/src/main/java/com/example/server/service/UsersService.java
@@ -1,4 +1,36 @@
 package com.example.server.service;
 
+import com.example.server.domain.users.Users;
+import com.example.server.dto.users.UsersResponseDto;
+import com.example.server.dto.users.UsersSignInRequestDto;
+import com.example.server.repository.UsersRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Slf4j
 public class UsersService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UsersRepository usersRepository;
+
+    public UsersResponseDto signIn(UsersSignInRequestDto requestDto) {
+
+
+        Users users = requestDto.toEntity();
+        String encodedPassword = passwordEncoder.encode(users.getPassword());
+        users.encodePassword(encodedPassword);
+        usersRepository.save(users);
+        return UsersResponseDto.builder()
+                .id(users.getId())
+                .email(users.getEmail())
+                .username(users.getName())
+                .nickname(users.getNickname())
+                .build();
+    }
 }
