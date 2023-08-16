f9071e4d64a20c10d8af5d135e5c4a154ad5b694
JooeonLee <cacophony0609@gmail.com>
2023-08-16 23:02:17 +0900

    Feat : TimeTableCourseDetailRepository 추가

--- server/src/main/java/com/example/server/repository/TimeTableRepository.java
+++ server/src/main/java/com/example/server/repository/TimeTableRepository.java
@@ -4,6 +4,9 @@ import com.example.server.domain.TimeTable;
 import com.example.server.domain.users.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TimeTableRepository extends JpaRepository<TimeTable, Long> {
 
+    Optional<TimeTable> findByUsersId(Long UsersId);
 }
