562ce5e31559194f8893423a3cf3d206fc11b9b8
JooeonLee <cacophony0609@gmail.com>
2023-08-17 08:16:20 +0900

    Feat : CourseReadResponseDto 추가

---
+++ server/src/main/java/com/example/server/dto/course/CourseReadResponseDto.java
@@ -0,0 +1,32 @@
+package com.example.server.dto.course;
+
+import com.example.server.domain.Course;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CourseReadResponseDto {
+
+    private Long courseId;
+    private String courseName;
+    private String professor;
+    private String categoryName;
+    private String courseDetail;
+    private String courseSummary;
+    private Long registerPeople;
+
+    public CourseReadResponseDto (Course course, Long registerPeople) {
+        this.courseId = course.getId();
+        this.courseName = course.getName();
+        this.professor = course.getUsers().getName();
+        this.categoryName = course.getCourseCategories().get(0).getCategory().getName();
+        this.courseDetail = course.getDetail();
+        this.courseSummary = course.getSummary();
+        this.registerPeople = registerPeople;
+    }
+}
