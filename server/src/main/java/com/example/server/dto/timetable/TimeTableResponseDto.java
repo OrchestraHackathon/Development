bc2dc2a5081afe1d0fcddf7df2c62d265773b690
JooeonLee <cacophony0609@gmail.com>
2023-08-16 17:56:18 +0900

    Feat : TimeTableResponseDto 생성

--- server/src/main/java/com/example/server/dto/timetable/TimeTableResponseDto.java
+++ server/src/main/java/com/example/server/dto/timetable/TimeTableResponseDto.java
@@ -1,4 +1,18 @@
 package com.example.server.dto.timetable;
 
+import com.example.server.domain.TimeTable;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class TimeTableResponseDto {
+
+    private String name;
+    private Long id;
+
+    public TimeTableResponseDto(TimeTable timeTable) {
+        this.name = timeTable.getName();
+        this.id = timeTable.getId();
+    }
 }
