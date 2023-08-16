489309993e9067e30748fea0f4dec7f5eae20f3f
JooeonLee <cacophony0609@gmail.com>
2023-08-16 19:46:14 +0900

    Add : CourseDeatilRepository 생성

---
+++ server/src/main/java/com/example/server/repository/CourseDetailRepository.java
@@ -0,0 +1,8 @@
+package com.example.server.repository;
+
+import com.example.server.domain.CourseDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseDetailRepository extends JpaRepository<CourseDetail, Long> {
+
+}
