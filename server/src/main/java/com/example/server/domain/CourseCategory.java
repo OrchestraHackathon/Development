0d85267102f19f5161e467e6006e01058f3b6364
JooeonLee <cacophony0609@gmail.com>
2023-08-17 02:56:29 +0900

    Feat : CourseCategory 도메인 추가

--- server/src/main/java/com/example/server/domain/CourseCategory.java
+++ server/src/main/java/com/example/server/domain/CourseCategory.java
@@ -1,4 +1,25 @@
 package com.example.server.domain;
 
-public class CourseCategory {
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class CourseCategory extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 연관 관계 Mapping
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
 }
